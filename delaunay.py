0ec93fccaceb31cdc226ae78dbf436f3c0bfd067
zamyk <zamyk@hotmail.com>
2024-12-22 20:03:12 +0100

    added some sources

--- delaunay.py
+++ delaunay.py
@@ -1,2 +1,18 @@
 def triangulate():
   pass
+
+
+
+
+
+
+
+
+# sources:
+# Some great thing about non-constrained delaunay https://ianthehenry.com/posts/delaunay/#fnref:2
+# constrained but in type script https://tchayen.com/constrained-delaunay-triangulation-from-a-paper
+# there is also chapter in 'the' book
+# https://cp-algorithms.com/geometry/delaunay.html
+# it seems there are few different options divida and conqure or online idk which one to use
+# http://www.geom.uiuc.edu/~samuelp/del_project.html
+# divide and conquer seems great dunno about constraining tho
