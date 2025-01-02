7792a06ed091688923cd2f39881e75c81809a166
zamyk <zamyk@hotmail.com>
2025-01-02 21:02:03 +0100

    snowflake

--- generate_sun_like_figure.py
+++ generate_sun_like_figure.py
@@ -47,3 +47,66 @@ def visualize(points):
     plt.show()
 
 # visualize(generate(1,6,10))
+
+class Vector2D:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+
+  def __add__(self, other):
+    return Vector2D(self.x + other.x, self.y + other.y)
+
+  def __sub__(self, other):
+    return Vector2D(self.x - other.x, self.y - other.y)
+
+  def __mul__(self, scalar):
+    return Vector2D(self.x * scalar, self.y * scalar)
+
+  def __truediv__(self, scalar):
+    return Vector2D(self.x / scalar, self.y / scalar)
+
+  def dot(self, other):
+    return self.x * other.x + self.y * other.y
+
+  def magnitude(self):
+    return (self.x**2 + self.y**2) ** 0.5
+
+  def normalize(self):
+    mag = self.magnitude()
+    if mag == 0:
+      return Vector2D(0, 0)
+
+  def rotateRight(self):
+    self.x, self.y = self.y, -self.x
+
+def genRegularPolygon(n, radius, offset = 0):
+  step = (2 * math.pi / n)
+  xs = [ math.cos(step * i + offset) * radius for i in range(n) ]
+  ys = [ math.sin(step * i + offset) * radius for i in range(n) ]
+  return xs, ys
+
+def snowflake(iterations, radius):
+  xs, ys = genRegularPolygon(3, radius)
+  points = [ Vector2D(xs[i], ys[i]) for i in range(len(xs)) ]
+
+  for i in range(iterations):
+    newPoints = []
+    for j in range(len(points)):
+      next = (j + 1) % len(points)
+
+      p1 = points[j]
+      p5 = points[next]
+      vect = p5 - p1
+      vect /= 3.
+      p2 = p1 + vect
+      perp = Vector2D(vect.x, vect.y)
+      perp.rotateRight()
+      perp *= math.sqrt(3) / 2
+      p3 = p2 + vect / 2. + perp
+      p4 =  p2 + vect
+      newPoints.append(p1)
+      newPoints.append(p2)
+      newPoints.append(p3)
+      newPoints.append(p4)
+    points = newPoints
+  return list(zip([p.x for p in points], [p.y for p in points]))
