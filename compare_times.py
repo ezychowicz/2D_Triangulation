54c931778e1fb277bb7d3c885c72d4888d92b807
Emil <emil.zychowicz@gmail.com>
2025-01-03 04:43:26 +0100

    juz koniec prawie

---
+++ compare_times.py
@@ -0,0 +1,22 @@
+import generate_sun_like_figure
+import delaunay
+import monotonic_division
+import time
+import numpy as np
+import utils.draw_triangulation
+
+
+
+for n in [2]:
+    print(f"n={n}")
+    points = generate_sun_like_figure.snowflake(n, 100)
+    start = time.time()
+    delaunay.triangulate(points)
+    # utils.draw_triangulation.draw(points,delaunay.triangulate(points))
+    end = time.time()
+    print(f"DELAUNAY: {end - start}")
+    start = time.time()
+    monotonic_division.triangulate(points)
+    utils.draw_triangulation.draw(points,monotonic_division.triangulate(points))
+    end = time.time()
+    print(f"MD: {end - start}")
